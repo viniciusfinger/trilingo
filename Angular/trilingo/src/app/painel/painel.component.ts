2721b2ec999fc8a63025c6b1b4273809afa4979b
FINGER-PESSOAL <finger-pessoal@cnx-viniciusfinger.lan>
2022-08-01 20:43:23 -0300

    Rounds system

--- Angular/trilingo/src/app/painel/painel.component.ts
+++ Angular/trilingo/src/app/painel/painel.component.ts
@@ -11,9 +11,15 @@ export class PainelComponent implements OnInit {
 
   public frases : Array<Frase> = FRASES;
   public instrucao : string = "Traduza a frase:";
-  public resposta : string;
+  public resposta : string = "";
 
-  constructor() { }
+  public rodada : number = 0;
+  public progresso : number = 0;
+  public rodadaFrase : Frase; 
+
+  constructor() {
+    this.atualizaRodada(); 
+  }
 
   ngOnInit() {
   }
@@ -23,7 +29,20 @@ export class PainelComponent implements OnInit {
   }
 
   public verificarResposta() : void {
-    console.log("verificar resposta:", this.resposta);    
+    
+    if (this.resposta == this.rodadaFrase.frasePt){
+      alert("Parabéns! você acertou!");
+
+      this.rodada++;
+      this.atualizaRodada();
+      this.progresso += (100 / this.frases.length);
+    } else {
+      alert("A frase está errada.");
+    } 
   }
 
+  private atualizaRodada(){
+    this.rodadaFrase = this.frases[this.rodada];
+    this.resposta = "";  
+  }
 }
